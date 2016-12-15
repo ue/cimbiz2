13f6f742786b06be194342047a42ab47fcca3301
ugrerdl <ugurerdal93@gmail.com>
2016-12-15 16:39:53 +0300

    fix

---
+++ Cimbiz2/obj/Release/Package/PackageTmp/Content/js/instagramapi.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/2965949423/media/recent/?access_token=2965949423.1677ed0.d04cfc1f2ae64390a23b3048910a55eb",
+        success: function (data) {
+            for (var i = 0; i < 50; i++) {
+                $(".instagramapi ul").append("<a data-lightbox='example-set' data-title='" + data.data[i].caption.text + "'  class='example-image-link hvr-grow-rotate' href='" + data.data[i].images.low_resolution.url + "'><img class='instimg ' src='" + data.data[i].images.low_resolution.url + "' alt='' /></a>");
+            }
+        }
+    });
+});
